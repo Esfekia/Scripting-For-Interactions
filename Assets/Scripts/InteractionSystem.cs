90d7c5e343f1184b01fa227746b251d1082cafc8
Sertac <sertac@gmail.com>
2022-05-28 12:25:02 -0400

    added explanatory comments to InteractionSystems.cs
    added explanatory comments to InteractionSystems.cs

--- Assets/Scripts/InteractionSystem.cs
+++ Assets/Scripts/InteractionSystem.cs
@@ -5,21 +5,24 @@ using UnityEngine.InputSystem;
 
 public class InteractionSystem : MonoBehaviour
 {
-    // The object that the raycast is currently hitting
+    // The container for referencing the object that the raycast is currently hitting
     public GameObject focusedObject;
     
     public GameObject pickupSlot;
 
+    // distance where player will be able to pick up things
     public float InteractDistance;
 
+
+    // boolean to see if player is holding something, this will affect what interact will do.
     public bool holding;
 
     private void FixedUpdate()
     {
-        // If player is holding an object, return
+        // if player is holding an object, no need to cast a ray, just return
         if(holding)        
             return;
-        // Compute the player's forward direction
+        // compute the player's forward direction
         Vector3 fwd = transform.TransformDirection(Vector3.forward);
 
         // hit var to store our results
@@ -32,10 +35,12 @@ public class InteractionSystem : MonoBehaviour
 
         if (Physics.Raycast(ray, out hit))
         {
+            // if we hit something, we have it in focus
             focusedObject = hit.collider.gameObject;
         }
         else
         {
+            // if we are not looking at anything, no focus object
             focusedObject = null;
         }
     }
